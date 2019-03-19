60b3b82b139d26e0eca7832e9725b211de630b07
santiart <47418108+santiart@users.noreply.github.com>
2019-03-19 15:25:54 -0300

    Add files via upload

---
+++ prog en c/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+
+    printf("Usted ingreso el numero %d", numero);
+
+    /*int numero;
+    int numero2;
+    char letra;
+    float altura;
+
+    numero = 10;
+    numero2 = 34;
+    letra = 'a';
+    altura = 23.5;
+
+    numero2 = altura;
+
+    printf("La variable altura vale %.1f y la letra vale %c", altura, letra);
+
+    /*printf("La variable altura vale %d", numero2);*/
+    /*printf("La variable altura vale %.0f", altura);*/
+    /*printf("La variable letra vale %c", letra);*/
+    /*printf("La variable numero vale %d y numero2 vale %d", numero, numero2);*/
+
+    return 0;
+}
